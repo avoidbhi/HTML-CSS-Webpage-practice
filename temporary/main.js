543821dc41b43b83bc92692c322024f5bde28598
Abhijeet Kanase <chillgoziecoder@outlook.com>
2023-06-24 18:15:59 +0530

    1.0

---
+++ temporary/main.js
@@ -0,0 +1,14 @@
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music','movies', 'sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+
+person.email = 'john@gmail.com';
+console.log(person);
